d4a960c88a1a9007b30b7f29b9681c1481d8ab2e
Kim Van Renterghem <kim.vanrenterghem@t4t.rocks>
2018-04-28 00:34:21 +0200

    creating When_having_input test

---
+++ diagrammatically.Domein.Test/When_having_input.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace diagrammatically.Domein.Test
+{
+    [TestClass]
+    public class When_having_input
+    {
+        [TestMethod]
+        public void Then_There_Will_Be_A_List_Of_Posibiletys()
+        {
+        }
+    }
+}
