bc75c3ce4706b7e042cb09dbeb82751559d834a6
Kim Van Renterghem <kim.vanrenterghem@t4t.rocks>
2018-04-29 09:56:52 +0200

    add fitst test

---
+++ diagrammatically.Domein.Test/Given_When_Then.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace diagrammatically.Domein.Test
+{
+    public abstract class Given_When_Then
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Given();
+            When();
+        }
+
+        protected abstract void Given();
+        protected abstract void When();
+    }
+}
--- diagrammatically.Domein.Test/When_having_input.cs
+++ diagrammatically.Domein.Test/When_having_input.cs
@@ -4,8 +4,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace diagrammatically.Domein.Test
 {
     [TestClass]
-    public class When_having_input
+    public class When_having_input : Given_When_Then
     {
+        protected override void Given()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void When()
+        {
+            throw new NotImplementedException();
+        }
+
         [TestMethod]
         public void Then_There_Will_Be_A_List_Of_Posibiletys()
         {
