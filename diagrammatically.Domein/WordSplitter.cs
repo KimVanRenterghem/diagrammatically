8d187e9995a1b12292aa658dab7ea62b6c7642f3
Kim <kim.vanrenterghem@gmail.com>
2018-06-26 17:43:58 +0200

    add kammelkasing word splitter

--- diagrammatically.Domein/WordSplitter.cs
+++ diagrammatically.Domein/WordSplitter.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace diagrammatically.Domein
 {
     public class WordSplitter : IInputProseser
     {
-        private IInputProseser _inputProseser;
+        private readonly IInputProseser _inputProseser;
+        private readonly char[] _splits = " _".ToCharArray();
+        private readonly char[] _kammels = "ABCDEFGHIJKLMNOPQRSTUVWYYZ".ToCharArray();
 
         public WordSplitter(IInputProseser inputProseser)
         {
@@ -15,10 +18,19 @@ namespace diagrammatically.Domein
         public void Loockup(string filter, IEnumerable<string> langs)
         {
             filter = filter
-                .Split(' ')
-                .Last()
-                .Split('_')
+                .Split(_splits)
                 .Last();
+
+            var index = _kammels
+                .Select(l => filter.LastIndexOf(l))
+                .Where(i => i > 0)
+                .OrderBy(i => i)
+                .LastOrDefault();
+
+            if (index > 0)
+            {
+                filter = filter.Substring(index);
+            }
             _inputProseser.Loockup(filter, langs);
         }
     }
